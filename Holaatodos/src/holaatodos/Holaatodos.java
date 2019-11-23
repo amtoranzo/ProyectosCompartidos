176438ba88a2cefe05210c570eab931c56ee238c
amoraltoranzo <amtoranzo@gmail.com>
2019-11-23 21:21:44 +0100

    Añado adios

--- Holaatodos/src/holaatodos/Holaatodos.java
+++ Holaatodos/src/holaatodos/Holaatodos.java
@@ -16,7 +16,7 @@ public class Holaatodos {
      */
     public static void main(String[] args) {
         System.out.println("Hola a todos");
-        
+        System.out.println("adios");
     }
     
 }
